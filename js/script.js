f73b9707beca4bb7ab5d8498a6e0f9bcf2232a47
Liza Usova <Liza.usova@gmail.com>
2025-03-20 12:11:14 +0200

    hw_6 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const firstUserInput = prompt('Enter first string');
+const secondUserInput = prompt('Enter second string');
+const thirdUserInput = prompt('Enter third string');
+
+console.log(firstUserInput + secondUserInput + thirdUserInput);
+
+
+const userNumberInput = prompt('Enter a five-digit number');
+
+const userNumberInputToArray = userNumberInput.split('');
+console.log(userNumberInputToArray.sort())
+
