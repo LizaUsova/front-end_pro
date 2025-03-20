c08ffe8241424b403a96a730d6e209d6405a8648
Liza Usova <Liza.usova@gmail.com>
2025-03-20 12:18:37 +0200

    hw_7 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+//Task #1
+
+const name = 'John';
+const admin = name;
+
+alert(admin);
+
+
+//Task #2
+
+const planetName = 'Earth';
+const userName = 'John';
+
+//Task #3
+
+const numberString = '10' + 10;
+console.log(typeof numberString);
