b476535232807db77cbb5a2c6342c3256d676dbb
Liza Usova <Liza.usova@gmail.com>
2025-03-21 12:43:05 +0200

    hw_9 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const userInput = prompt('Enter value and C or F');
+
+const userNumber = parseInt(userInput);
+const letterMatch = userInput.match(/[a-zA-Z]/);
+const userLetter = letterMatch ? letterMatch[0].toLowerCase() : null;
+
+if (userLetter === 'c') {
+    alert(`From C to F: ${(userNumber * 9/5) + 32}`);
+} else if (userLetter === 'f') {
+    alert(`From C to F: ${(userNumber - 32) * 5 / 9}`);
+} else {
+    alert('Enter correct information please')
+};
