efcf87a806e4f065470767dd8706423ba0b31df0
Liza Usova <Liza.usova@gmail.com>
2025-03-21 19:13:04 +0200

    hw_11 else_if completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const userInputOperator = prompt('Enter mathematical operator');
+const firstUserNumber = +prompt('Enter first number');
+const secondUserNumber = +prompt('Enter second number');
+
+
+if (isNaN(firstUserNumber) || isNaN(secondUserNumber)) {
+    alert('Please enter valid numbers!');
+} else {
+    let resultAdd = null;
+    let resultSub = null;
+    let resultMult = null;
+    let resultDiv = null;
+
+    if (userInputOperator === '+') {
+        resultAdd = firstUserNumber + secondUserNumber;
+        console.log(`${firstUserNumber} + ${secondUserNumber} = ${resultAdd}`);
+    }
+
+    if (userInputOperator === '-') {
+        resultSub = firstUserNumber - secondUserNumber;
+        console.log(`${firstUserNumber} - ${secondUserNumber} = ${resultSub}`);
+    }
+
+    if (userInputOperator === '*') {
+        resultMult = firstUserNumber * secondUserNumber;
+        console.log(`${firstUserNumber} * ${secondUserNumber} = ${resultMult}`);
+    }
+
+    if (userInputOperator === '/') {
+        resultDiv = firstUserNumber / secondUserNumber;
+        console.log(`${firstUserNumber} / ${secondUserNumber} = ${resultDiv}`);
+    }
+}
