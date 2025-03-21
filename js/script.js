cda3d1f38dce726eb4aa378a00e1239b3afaa2d9
Liza Usova <Liza.usova@gmail.com>
2025-03-21 18:45:04 +0200

    hw_10 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const firstUserOperand = +prompt('Enter first operand');
+const secondUserOperand = +prompt('Enter second operand');
+
+if (firstUserOperand && secondUserOperand) {
+    const add = firstUserOperand + secondUserOperand;
+    const sub = firstUserOperand - secondUserOperand;
+    const multiply = firstUserOperand * secondUserOperand;
+    const divide =  firstUserOperand / secondUserOperand;
+
+    console.log(`Addition: ${firstUserOperand} + ${secondUserOperand} = ${add} \n
+                 Subtraction: ${firstUserOperand} - ${secondUserOperand} = ${sub} \n
+                 Multiplication: ${firstUserOperand} * ${secondUserOperand} = ${multiply} \n
+                 Division: ${firstUserOperand} / ${secondUserOperand} = ${divide}`);
+} else {
+    alert('is not a number, write correct please');
+};
+
