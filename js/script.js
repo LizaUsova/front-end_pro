c2ba28175e45451035a44d4ff8e2b4d36c9913b0
Liza Usova <Liza.usova@gmail.com>
2025-03-21 12:05:16 +0200

    hw_8 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const age = +prompt('How old are you?');
+const plusAge = 10;
+
+const futureAge = age + plusAge;
+
+alert(`in ${plusAge} years you will be ${futureAge} years old`);
