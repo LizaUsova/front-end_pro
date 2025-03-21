2579cdf35e7cf5e2e5d5d3bf35994898e6b64bc1
Liza Usova <Liza.usova@gmail.com>
2025-03-21 18:32:08 +0200

    hw_9 completed fixed letter mistake

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const userLetter = letterMatch ? letterMatch[0].toLowerCase() : null;
 if (userLetter === 'c') {
     alert(`From C to F: ${(userNumber * 9/5) + 32}`);
 } else if (userLetter === 'f') {
-    alert(`From C to F: ${(userNumber - 32) * 5 / 9}`);
+    alert(`From F to C: ${(userNumber - 32) * 5 / 9}`);
 } else {
     alert('Enter correct information please')
 };
