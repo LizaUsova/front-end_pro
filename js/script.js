dd74f26b84f4856095fa5c989f1faea673842e57
Liza Usova <Liza.usova@gmail.com>
2025-03-22 12:57:26 +0200

    hw_13 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const firstUserInput = +prompt('Enter first value');
+const secondUserInput = +prompt('Enter second value');
+
+if(isNaN(firstUserInput) || isNaN(secondUserInput)) {
+    alert('Enter valid values please')
+} else {
+    alert(`Користувач ввів ${firstUserInput} і ${secondUserInput}: \n
+    ${firstUserInput} + ${secondUserInput} = ${firstUserInput+secondUserInput} \n
+    ${firstUserInput} - ${secondUserInput} = ${firstUserInput-secondUserInput} \n
+    ${firstUserInput} * ${secondUserInput} = ${firstUserInput*secondUserInput} \n
+    ${firstUserInput} / ${secondUserInput} = ${firstUserInput/secondUserInput}`)
+}
