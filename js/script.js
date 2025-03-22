d07c2957a0d3945a2976140f43fb3b3cdace6b38
Liza Usova <Liza.usova@gmail.com>
2025-03-22 13:02:04 +0200

    hw_15 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const firstUserInput = +prompt('Enter first value');
+const secondUserInput = +prompt('Enter second value');
+const thirdUserInput = +prompt('Enter third value');
+
+if (isNaN(firstUserInput) || isNaN(secondUserInput) || isNaN(thirdUserInput)) {
+    alert('enter valid value')
+} else {
+    alert(`${(firstUserInput+secondUserInput+thirdUserInput)/3}`)
+}
