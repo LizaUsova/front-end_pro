5468e906e402e8d19d22894db539555aa8f9e312
Liza Usova <Liza.usova@gmail.com>
2025-03-22 22:44:24 +0200

    hw_20 task2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 'use strict';
+//1
+let x = 10;
+let y = 7;
+
+const compare = x > y ? 'x більше за y' : 'x не більше, ніж y';
+
+
+//2
+
+let userInput = prompt('Enter integer number');
+
+if(isNaN(userInput) || userInput.trim() === '') {
+    alert('Please enter a valid integer number');
+} else {
+    userInput = Number(userInput);
+    let userInputAbs = Math.abs(userInput);
+    let userInputLength = userInputAbs.toString().length;
+
+    if (userInput > 0) {
+        alert(`Число ${userInput} позитивне та має ${userInputLength} знаків`);
+    } else if (userInput < 0) {
+        alert(`Число ${userInput} негативне та має ${userInputLength} знаків`);
+    } else {
+        alert('Ви ввели 0, яке є нейтральним числом');
+    }
+}
+
+//3
+
+
