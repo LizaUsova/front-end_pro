3ffe1c8f406b003828c221969036b61f147bb5d5
Liza Usova <Liza.usova@gmail.com>
2025-03-22 22:10:09 +0200

    hw_19 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let userInput = prompt('Enter your age');
+
+if(isNaN(userInput) || userInput === null || userInput.trim() === '') {
+    alert('Enter number please');
+} else {
+    userInput = Number(userInput);
+
+    if(userInput === 0 || userInput >= 5) {
+    alert(`Тобі ${userInput} років`);
+    } else if(userInput === 1) {
+        alert(`Тобі ${userInput} рік`);
+    } else if(userInput >= 2 && userInput <= 4) {
+        alert(`Тобі ${userInput} роки`);
+    }
+}
+
--- readme.md
+++ readme.md
@@ -1,1 +1,14 @@
-### Homework 1 ###
+### Homework 18 ###
+
+let numOrStr = prompt('input number or string');
+console.log(numOrStr)
+
+if(numOrStr === null) {
+console.log('ви скасували')
+} else if( numOrStr.trim() === '' ) {
+console.log('Empty String');
+} else if ( isNaN( +numOrStr ) ) {
+console.log(' number is Ba_NaN')
+} else {
+console.log('OK!')
+}
