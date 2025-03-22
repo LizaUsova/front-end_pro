286454e9ce5add6c2755125085e02927906cfd32
Liza Usova <Liza.usova@gmail.com>
2025-03-22 11:07:06 +0200

    hw_12 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const user = {};
+
+user.name = 'John';
+user.surname = 'Smith';
+console.log(user.name);
+
+user.name = 'Pete';
+console.log(user.name);
+
+delete user['name'];
+
+console.log(user);
+
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+const sum = salaries.Pete + salaries.Ann + salaries.Pete;
+console.log(sum);
