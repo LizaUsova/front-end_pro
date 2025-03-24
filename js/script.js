bfb823702d87d467c30c24efdfb97101f651a9ec
Liza Usova <Liza.usova@gmail.com>
2025-03-24 18:17:10 +0200

    hw_24 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const arr = [1, 2, 3, 4, 5];
+let add = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    add += arr[i];
+}
+
+console.log(add)
+
+let sum = 0;
+const degree = 2;
+
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i] ** degree;
+}
+
+console.log(sum);
