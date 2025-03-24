39ebf20ec8964733411e3d87245ac721928b1da7
Liza Usova <Liza.usova@gmail.com>
2025-03-24 23:23:12 +0200

    hw_27 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+
+
+const generateKey = function (length, characters) {
+    let str = '';
+
+    for(let i = 0; i < length; i++) {
+        const random = Math.floor(Math.random() * characters.length);
+        str += characters[random];
+    }
+    return  str;
+}
+
+const key = generateKey(16, characters);
+console.log(key);
--- readme.md
+++ readme.md
@@ -1,1 +1,11 @@
-### Homework 1 ###
+### Homework 27 ###
+
+Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
+
+Наприклад:
+
+Copy code
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const key = generateKey(16, characters);
+console.log(key); // eg599gb60q926j8i
