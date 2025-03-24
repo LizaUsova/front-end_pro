40f3c7db02d3bad88c35136c48dcc5ac71e7a3e6
Liza Usova <Liza.usova@gmail.com>
2025-03-24 23:46:41 +0200

    hw_28 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const userInputNumber = +prompt('Enter some number');
+const userInputPower = +prompt('Enter power of number');
+
+const exponentiation = function (number, power = 1) {
+    if (isNaN(number) || isNaN(power)) {
+        return 'Arguments are not numbers';
+    } else {
+        return number ** power;
+    }
+}
+
+const result = alert(exponentiation(userInputNumber, userInputPower));
