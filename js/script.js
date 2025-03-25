b1c3bc745a7e67dec92922bf0b2e01428e979a0a
Liza Usova <Liza.usova@gmail.com>
2025-03-25 17:23:06 +0200

    hw_29 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const padString = function (str, number = 1, symbol ='*', add = true) {
+
+    if(typeof str !== 'string' || str.length === 0) {
+        return  'помилка, не введено рядок';
+    }
+
+    if(typeof number !== 'number' || isNaN(number)) {
+        return  'помилка, не введено число';
+    }
+
+    if(typeof symbol !== 'string' || symbol.length !== 1) {
+        return  'помилка, не введено символ';
+    }
+
+    if(typeof add !== 'boolean') {
+        return  'помилка, не введено додавати символи зліва чи справа';
+    } else
+
+    if(number < str.length) {
+        return str.substring(0, number);
+    }
+
+    let padding = symbol.repeat(number - str.length);
+    return add ? str + padding : padding + str;
+}
+
+console.log(padString('hello', 8));
+console.log(padString('hello', 6, '*', false));
+console.log(padString('hello', 2));
+
+
