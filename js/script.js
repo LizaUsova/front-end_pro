a616719b2e6bd072f4062769eca646a7d76b19d1
Liza Usova <Liza.usova@gmail.com>
2025-03-25 17:44:14 +0200

    hw_30 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+const newArrFunc = function (arr) {
+    const currentPositiveNumber = [];
+
+    if(arr.length === 0) {
+        return 'array is empty';
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > 0) {
+            currentPositiveNumber.push(arr[i]);
+        }
+    }
+
+    return currentPositiveNumber.length ? currentPositiveNumber : null;
+}
+
+const result = newArrFunc(arr);
+console.log(result);
