00b644ff672d105c85be3ddb4da4f95c4b0f43a8
Liza Usova <Liza.usova@gmail.com>
2025-04-07 01:06:41 +0300

    hw_52 tasks 1-2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+//1
+function factorial (n) {
+    if(n === 1 || n === 0) {
+        return 1
+    }     else {
+        return n * factorial(n - 1)
+    }
+}
+
+console.log(factorial(4));
+
+//2
+function pow(num, degree) {
+    if(degree === 0) {
+        return 1
+    } else if (degree > 0) {
+        return num * pow(num, degree - 1);
+    }
+}
+
+console.log(pow(2, 6));
