25751345d4c1139dbdf40b265c0bac3b9508f184
Liza Usova <Liza.usova@gmail.com>
2025-04-06 10:35:19 +0300

    hw_39 tasks 1,2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+//1
+const a = [3, 5, 8, 4, 10, 2, 5, 3];
+const b = ['hi', 'hello'];
+
+const indexOf = function (arr, item) {
+    let res = -1;
+    for(let i  = 0; i < arr.length; i++) {
+        if(arr[i] === item) {
+            return i;
+        }
+    }
+
+    return res;
+}
+
+const myA = indexOf(a, 8);
+const myB = indexOf(b, 'hi');
+console.log(myA);
+console.log(myB);
+
+//2
+const lastIndexOf = function (arr, item) {
+    let res = -1;
+    for(let i = arr.length - 1; i >= 0; i--) {
+        if(arr[i] === item) {
+            return i;
+        }
+    }
+
+    return res;
+}
+
+const myALast = lastIndexOf(a, 3);
+const myBLast = lastIndexOf(b, 'hi');
+console.log(myALast);
+console.log(myBLast);
