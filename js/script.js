fcc71c10ecb321ec325149e2edf66dc319a434a4
Liza Usova <Liza.usova@gmail.com>
2025-04-08 16:44:12 +0300

    hw_56 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+const user = {
+    name: 'Vova',
+    lastName: 'Doe',
+    age: 27
+}
+
+const apply = function (func, ctx, arg = []) {
+    const innerCtx = structuredClone(ctx);
+    innerCtx.cM = func
+    return innerCtx.cM(...arg)
+}
+
+const display = function () {
+    console.log(this.name)
+}
+
+const result = apply(display, user)
