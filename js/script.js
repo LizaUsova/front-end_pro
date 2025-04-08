2a54b7c8ed63416c11daa64f8870e197f0fefbea
Liza Usova <Liza.usova@gmail.com>
2025-04-08 16:30:06 +0300

    hw_54 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const user = {
+    name: 'Vova',
+    lastName: 'Doe',
+    age: 27
+}
+
+const call = function (func, ctx, arg) {
+    const innerCtx = structuredClone(ctx);
+    innerCtx.cM = func
+    innerCtx.cM(arg)
+}
+
+const display = function () {
+    console.log(this.name)
+}
+
+const bind = function (bindFunc, bindCtx, bindArg) {
+    return function () {
+        return call(bindFunc, bindCtx, bindArg)
+    }
+}
+
+const b = bind(display, user)
+console.log(b);
+b();
