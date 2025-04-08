1697e778b4702861c321644b5c02074fb151fb69
Liza Usova <Liza.usova@gmail.com>
2025-04-08 23:26:26 +0300

    hw_57 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+(function () {
+    const sum = (param) => {
+        let result = param;
+
+        return (number) => {
+            result += number;
+            return result
+        }
+    }
+
+
+    const example = sum(3);
+    console.log(example(5));
+    console.log(example(20));
+})()
