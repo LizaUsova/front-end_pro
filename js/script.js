e95571dcf5e1de2dd6fdf228d484b1ea5dad24be
Liza Usova <Liza.usova@gmail.com>
2025-04-09 15:21:28 +0300

    hw_60 start

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 'use strict';
+const listUl = document.getElementById("ulId");
+const listElements = listUl.childNodes;
+
+const listElementsArray = []
+
+for (const elem of listElements) {
+    if (elem instanceof HTMLElement) {
+        listElementsArray.push(elem)
+    }
+}
+
+console.log(listElementsArray);
+console.log(listElementsArray.length);
+
+const textElemArray = [];
+
+for(const elem of listElementsArray) {
+    textElemArray.push(elem.textContent)
+}
+
+console.log(textElemArray);
