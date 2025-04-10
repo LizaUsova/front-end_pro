cb63ac1c6ba3e29791ea6c5d1d0b3a4609fcf4ed
Liza Usova <Liza.usova@gmail.com>
2025-04-10 21:37:00 +0300

    hw_62 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+(function (){
+    const arr = [1,2,3];
+    const arr1 = [1, 2, 3, [1.1, 1.2, 1.3], 4];
+
+    const generateList = (array) => {
+        const  ul = document.createElement('ul');
+
+         for(let i = 0; i < array.length; i++) {
+             let li = document.createElement('li');
+
+             if(Array.isArray(array[i])) {
+                 li.appendChild(generateList(array[i]))
+             } else {
+                 li.innerHTML = array[i]
+             }
+
+             ul.appendChild(li)
+         }
+
+     return ul;
+    }
+
+    document.body.appendChild(generateList(arr));
+    document.body.appendChild(generateList(arr1));
+
+})()
