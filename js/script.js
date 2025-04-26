79cc84d72939c3cc31e6a2c04a97d75da90e83f6
Liza Usova <Liza.usova@gmail.com>
2025-04-26 18:24:14 +0300

    hw_69 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+(function () {
+    let user = {
+        data: {
+            a: 1,
+            b: 2,
+            c: 3,
+            d: {
+                a1: 1,
+                b1: 2,
+                c1: 3,
+                d1: {
+                    a2: 3,
+                    b2: 3,
+                    c2: 3
+                }
+            }
+        }
+    }
+
+    const deepFreeze = (obj) => {
+        Object.freeze(obj)
+        for(let key in obj) {
+            const value = obj[key]
+            if(typeof value === 'object' && value !== null) {
+                deepFreeze(value);
+            }
+        }
+    }
+
+    deepFreeze(user)
+    console.log(Object.getOwnPropertyDescriptors(user));
+}())
