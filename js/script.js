fb13f21b1cd8dbbcb13e80a3f51a752a80a3adb4
Liza Usova <Liza.usova@gmail.com>
2025-04-26 18:02:15 +0300

    hw_68 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 'use strict';
+const arr = [1, 2, 3]
+
+const getItrObject = (itrDataStructure) => {
+    const dataArr = Array.from(itrDataStructure);
+    let currentIndex = 0;
+
+    return {
+        next() {
+            let done = false;
+
+            if(currentIndex + 1 > dataArr.length) {
+                done = true;
+            }
+
+            const result = {
+                value: dataArr[currentIndex],
+                done
+            }
+
+            currentIndex +=1;
+
+            return result
+        }
+    }
+}
+
+let itr = getItrObject(arr)
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
